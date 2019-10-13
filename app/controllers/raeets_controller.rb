3c3be0f0767523321a54499278130edfe448c02c
kakeru <okinawan.kkr0713@icloud.com>
2019-10-13 18:38:04 +0900

    controller/indexとview/index再記述

--- app/controllers/raeets_controller.rb
+++ app/controllers/raeets_controller.rb
@@ -1,14 +1,21 @@
 class RaeetsController < ApplicationController
 
   def index
+    @raeets =  Raeet.all
   end
 
-  def new
-    @raeet = Raeet.new
-  end
-
-  def create
-    
-
-  end
+  # def new
+  #   @raeet = Raeet.new
+  # end
+  #
+  # def create
+  #   Raeet.create(raeet_params)
+  #   redirect_to new_raeet_path
+  # end
+  #
+  # private
+  #
+  # def raeet_params
+  #   params.require(:raeet).permit(:content)
+  # end
 end
