2f3cd8dd95a7a737fca077def4de4dc51a9aed18
kakeru <okinawan.kkr0713@icloud.com>
2019-10-14 12:33:06 +0900

    helper/raeets_helperにアクション記述

--- app/helpers/raeets_helper.rb
+++ app/helpers/raeets_helper.rb
@@ -1,2 +1,9 @@
 module RaeetsHelper
+  def choose_new_or_edit
+    if action_name == 'new' || action_name == 'confirm'|| action_name == 'create'
+      confirm_raeets_path
+    elsif action_name == 'edit'
+      raeet_path
+    end
+  end
 end
