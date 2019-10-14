6af3b8faf564ba924f38b2442f10c430eaf7c7d7
kakeru <okinawan.kkr0713@icloud.com>
2019-10-14 10:17:21 +0900

    cntroller/befor_action & set_raeet記述(共通処理のメソッド化)

--- app/controllers/raeets_controller.rb
+++ app/controllers/raeets_controller.rb
@@ -1,4 +1,5 @@
 class RaeetsController < ApplicationController
+  before_action :set_raeet, only: [ :show, :edit, :update, :destroy]
 
   def index
     @raeets =  Raeet.all
@@ -18,15 +19,12 @@ class RaeetsController < ApplicationController
   end
 
   def show
-    @raeet = Raeet.find(params[:id])
   end
 
   def edit
-    @raeet = Raeet.find(params[:id])
   end
 
   def update
-    @raeet = Raeet.find(params[:id])
     if @raeet.update(raeet_params)
       redirect_to raeets_path, notice: "Raeetしました！"
     else
@@ -38,5 +36,9 @@ class RaeetsController < ApplicationController
 
   def raeet_params
     params.require(:raeet).permit(:content)
+  end
+
+  def set_raeet
+    @raeet = Raeet.find(params[:id])
   end
 end
